832e56488094b0500aa8c5de884c1c257b860207
megrez-lbj <1325349854@qq.com>
2025-05-04 16:58:15 +0000

    feat: 微调小试

--- RWKV-v5/demo-training-run.sh
+++ RWKV-v5/demo-training-run.sh
@@ -16,7 +16,7 @@ MODEL_TYPE="x070" # x060 => rwkv-6.0
 N_LAYER="24"
 N_EMBD="2048"
 #
-CTX_LEN="4096" # !!! change magic_prime if you change ctx_len !!!
+CTX_LEN="512" # !!! change magic_prime if you change ctx_len !!!
 PROJ_DIR="out/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE # set output folder
 #
 #######################################################################################################################
@@ -27,11 +27,11 @@ PROJ_DIR="out/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE # set output folder
 # Larger model => use smaller LR
 # Finetuning => use very small LR, such as 1e-5
 #
-M_BSZ="4" # takes ~9G VRAM here => reduce this to save VRAM, increase this for faster speed
+M_BSZ="32" # takes ~9G VRAM here => reduce this to save VRAM, increase this for faster speed
 LR_INIT="6e-4"
 LR_FINAL="6e-5"
 GRAD_CP=1 # 1 => slower, save VRAM; 0 => faster, more VRAM
-EPOCH_SAVE=10 # save every 10 "miniepochs" (1 miniepoch = 40320 * ctx_len tokens) => decrease if your GPU is weak
+EPOCH_SAVE=5 # save every 10 "miniepochs" (1 miniepoch = 40320 * ctx_len tokens) => decrease if your GPU is weak
 #
 #######################################################################################################################
 #
@@ -39,13 +39,13 @@ EPOCH_SAVE=10 # save every 10 "miniepochs" (1 miniepoch = 40320 * ctx_len tokens
 # use https://www.dcode.fr/prime-numbers-search
 #
 N_NODE=1 # number of nodes
-GPU_PER_NODE=1 # number of GPUs per node
+GPU_PER_NODE=2 # number of GPUs per node
 #
 DS_BUCKET_MB=2 # set to 2 for consumer GPUs, set to 200 for A100 / H100 (affects speed & vram usage)
 #
-python train.py --load_model "1" --wandb "" --proj_dir $PROJ_DIR --my_testing $MODEL_TYPE \
+python train.py --load_model "0" --wandb "RWKV7-L24-N2048" --proj_dir $PROJ_DIR --my_testing $MODEL_TYPE \
  --ctx_len $CTX_LEN --my_pile_stage 3 --epoch_count 999999 --epoch_begin 0 \
- --data_file "/root/RWKV-LM/RWKV-v5/data/test" --my_exit_tokens 64836 --magic_prime 11 \
+ --data_file "data/minipile" --my_exit_tokens 1498226207 --magic_prime 2926181 \
  --num_nodes $N_NODE --micro_bsz $M_BSZ --n_layer $N_LAYER --n_embd $N_EMBD --pre_ffn 0 --head_qk 0 \
  --lr_init $LR_INIT --lr_final $LR_FINAL --warmup_steps 10 --beta1 0.9 --beta2 0.99 --adam_eps 1e-18 --my_pile_edecay 0 --data_type "binidx" --vocab_size 65536 \
  --weight_decay 0.001 --epoch_save $EPOCH_SAVE --head_size_a 64 \
